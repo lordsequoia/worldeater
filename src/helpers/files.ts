154752f13106d3e24683d191c5c5243b6630abf4
LordSequoia <thelordsequoia@gmail.com>
2022-08-14 15:57:16 +0000

    wip

--- src/helpers/files.ts
+++ src/helpers/files.ts
@@ -2,6 +2,8 @@ import { createEffect, createEvent, createStore } from "effector"
 import { promises, readJson } from "fs-extra"
 import {parse, join} from 'path'
 import { Tail } from "tail"
+import chokidar from 'chokidar'
+import { splitMap } from "patronum"
 
 export const loadJsonFile = async <T>(filePath: string): Promise<T> => {
     const fileData = await readJson(filePath)
@@ -56,4 +58,40 @@ export const watchFile = (filePath: string) => {
     })
 
     return {$lines, addLine, stopWatching }
+}
+
+export type FileWatcherEventName = 
+    | 'add'
+    | 'addDir'
+    | 'change'
+    | 'unlink'
+    | 'unlinkDir'
+
+export const watchDir = (rootDir: string) => {
+    const fileEvent = createEvent<{eventName: FileWatcherEventName, path: string, fullPath: string}>()
+
+    const {fileAdded, fileChanged, fileRemoved, directoryAdded, directoryRemoved} = splitMap({
+        source: fileEvent,
+        cases: {
+            fileAdded: ({eventName, path, fullPath}) => eventName !== 'add' ? undefined : {path, fullPath},
+            fileChanged: ({eventName, path, fullPath}) => eventName !== 'change' ? undefined : {path, fullPath},
+            fileRemoved: ({eventName, path, fullPath}) => eventName !== 'unlink' ? undefined : {path, fullPath},
+
+            directoryAdded: ({eventName, path, fullPath}) => eventName !== 'addDir' ? undefined : {path, fullPath},
+            directoryRemoved: ({eventName, path, fullPath}) => eventName !== 'unlinkDir' ? undefined : {path, fullPath},
+        }
+    })
+
+    const $files = createStore<string[]>([] as string[])
+        .on(fileAdded, (files, file) => [...files, file.path])
+        .on(fileRemoved, (files, file) => files.filter(x => x !== file.path))
+
+
+    const watcher = chokidar.watch('.', {cwd: rootDir})
+
+    const stopWatching = watcher.on('all', (eventName, path) => {
+        fileEvent({eventName, path, fullPath: join(rootDir, path)})
+    })
+
+    return {$files, fileEvent, fileAdded, fileChanged, fileRemoved, directoryAdded, directoryRemoved, stopWatching}
 }
