fd07088d404802dd3755a7953b9674ede77db11b
LordSequoia <thelordsequoia@gmail.com>
2022-08-14 18:29:22 +0000

    fix

--- examples/streams.js
+++ examples/streams.js
@@ -5,7 +5,7 @@ const config = require('./config')
 const startStreamsDemo = async () => {
     const world = useWorldEater(config.projectDir, config.levelName)
 
-    world.serverLogs.addServerLog.watch(({timestamp, content}) => {
+    world.serverLogs.serverLogs.addServerLog.watch(({timestamp, content}) => {
         app.info(`[${timestamp}] ${content}`)
     })
 
