5be15fd29acc098b8351b0a77fd7eec08c2ca6bf
LordSequoia <thelordsequoia@gmail.com>
2022-08-15 21:33:08 +0000

    wip

--- scripts/eat-world.js
+++ scripts/eat-world.js
@@ -1,11 +1,22 @@
-const {useWorldEater, logger} = require('..')
+const {useWorldEater, useSockets, makeLogger} = require('..')
+
+const logger = makeLogger('eat-world-script')
 
 const eatWorld = () => {
-    const world = useWorldEater('/home/ubuntu/.swarms/default', 'world')
+    const world = useWorldEater('/home/ubuntu/.swarms/default', 'world', '0.0.0.0', 3083)
 
-    world.sockets.ioClient.on('info', (message) => {
+    const {createClient} = useSockets({
+        host: '127.0.0.1',
+        port: 3083
+    })
+
+    const {ioClient} = createClient()
+    
+    ioClient.on('info', (message) => {
         logger.info(`eat -> ${message}`)
     })
+
+    world.init()
 }
 
 eatWorld()
